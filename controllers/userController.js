45834ba5e7c7ec453e1a8be753b508fcab593242
bahati584 <isaacbaha584@gmail.com>
2025-08-01 10:34:29 +0300

    addedchanges

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ export const createUser = async (req, res) => {
   try {
     const { name, phone } = req.body;
 
-    const newUser = new User({ name, phone });
+    const newUser = new User({ name, email: phone });
     await newUser.save();
 
     res.status(201).json({ message: 'User created successfully', user: newUser });
@@ -59,7 +59,7 @@ export const updateUser = async (req, res) => {
     const { name, phone } = req.body;
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
-      { name, phone },
+      { name, email: phone },
       { new: true }
     );
     if (!updatedUser) return res.status(404).json({ message: 'User not found' });
